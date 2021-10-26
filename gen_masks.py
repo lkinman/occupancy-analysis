e85b21caeb7506246dbeed23420100ed587a9174
Laurel Kinman <47401759+lkinman@users.noreply.github.com>
2021-10-26 14:56:52 -0400

    Update and rename mrc2mask.py to gen_masks.py

--- gen_masks.py
+++ gen_masks.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 def add_args(parser):
-    parser.add_argument('--pdb', required = True, help = 'PDB file used for creating the mask')
+    parser.add_argument('--mrc', required = True, help = 'MRC file used for creating the mask')
     parser.add_argument('--outdir', type = str, required = True, help='Output directory to store the masks in')
     parser.add_argument('--extend', type = str, help = 'Number of angstroms to extend initial mask by')
     return parser
@@ -9,11 +9,11 @@ def add_args(parser):
 def main(args):
     if not os.path.exists(args.outdir):
         os.mkdir(args.outdir)
-    mask_name = str(args.outdir) + '/Mask_' + str(args.pdb).split('/')[-1]
+    mask_name = str(args.outdir) + '/Mask_' + str(args.mrc).split('/')[-1]
     if not args.extend:
-	    os.system('relion_mask_create --i ' + args.pdb + ' --o ' + mask_name)
+	    os.system('relion_mask_create --i ' + args.mrc + ' --o ' + mask_name)
     else:
-        os.system('relion_mask_create --i ' + args.pdb + ' --o ' + mask_name + ' --extend_inimask ' + args.extend)
+        os.system('relion_mask_create --i ' + args.mrc + ' --o ' + mask_name + ' --extend_inimask ' + args.extend)
     return
 
 if __name__ == '__main__':

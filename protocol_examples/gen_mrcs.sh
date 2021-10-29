f1db194995b93844c70a422e5ac745c81c6d4fe8
Laurel Kinman <47401759+lkinman@users.noreply.github.com>
2021-10-29 12:37:02 -0400

    Add files via upload

--- protocol_examples/gen_mrcs.sh
+++ protocol_examples/gen_mrcs.sh
@@ -1,1 +1,7 @@
-
+python gen_mrcs.py --map 00_aligned/reconstruct_000000/vol_084.mrc --pdb 00_aligned/prots1.pdb --chains abcdefghijklmnopqrstuvwxyz --res 5 --out 01_PDB_mrc --scriptdir 01_chimera_scripts 
+python gen_mrcs.py --map 00_aligned/reconstruct_000000/vol_084.mrc --pdb 00_aligned/prots2.pdb --chains abc --res 5 --out 01_PDB_mrc --scriptdir 01_chimera_scripts 
+python gen_mrcs.py --map 00_aligned/reconstruct_000000/vol_084.mrc --pdb 00_aligned/RNA_5S.pdb --chains abcde --res 5 --out 01_PDB_mrc --scriptdir 01_chimera_scripts 
+python gen_mrcs.py --map 00_aligned/reconstruct_000000/vol_084.mrc --pdb 00_aligned/RNA1.pdb --chains abcdefghijklmnopqrstuvwxyz --res 5 --out 01_PDB_mrc --scriptdir 01_chimera_scripts 
+python gen_mrcs.py --map 00_aligned/reconstruct_000000/vol_084.mrc --pdb 00_aligned/RNA2.pdb --chains abcdefghijklmnopqrstuvwxyz --res 5 --out 01_PDB_mrc --scriptdir 01_chimera_scripts 
+python gen_mrcs.py --map 00_aligned/reconstruct_000000/vol_084.mrc --pdb 00_aligned/RNA3.pdb --chains abcdefghijklmnopqrstuvwxyz --res 5 --out 01_PDB_mrc --scriptdir 01_chimera_scripts 
+python gen_mrcs.py --map 00_aligned/reconstruct_000000/vol_084.mrc --pdb 00_aligned/RNA4.pdb --chains abcdefghijklmnopqrstuvwx --res 5 --out 01_PDB_mrc --scriptdir 01_chimera_scripts
